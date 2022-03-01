e56b3f8727716acc69dcececf30ae80a9438f8c2
BudyBuilding <90955596+BudyBuilding@users.noreply.github.com>
2022-03-01 17:43:03 +0100

    Add files via upload

--- program.cpp
+++ program.cpp
@@ -1,93 +1,38 @@
-/*
-    g++ program.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o program `fltk-config --ldflags --use-images`
-*/
-#include "Simple_window.h"
-#include "Graph.h"
+#include "res.h"
+//#include "std_lib_facilities.h"
 
+/*
+
+Define a class B1 with a virtual function vf() and a non-virtual function f().
+Define both of these functions within class B1. Implement each function to
+output its name (e.g., B1::vf()). Make the functions public. Make a B1
+object and call each function.
+2. Derive a class D1 from B1 and override vf(). Make a D1 object and call
+vf() and f() for it.
+3. Define a reference to B1 (a B1&) and initialize that to the D1 object you
+just defined. Call vf() and f() for that reference.4. Now define a function called f() for D1 and repeat 1–3. Explain the results.
+5. Add a pure virtual function called pvf() to B1 and try to repeat 1–4.
+Explain the result.
+6. Define a class D2 derived from D1 and override pvf() in D2. Make an
+object of class D2 and invoke f(), vf(), and pvf() for it.
+7. Define a class B2 with a pure virtual function pvf(). Define a class D21
+with a string data member and a member function that overrides pvf();
+D21::pvf() should output the value of the string. Define a class D22 that is
+just like D21 except that its data member is an int. Define a function f() that
+takes a B2& argument and calls pvf() for its argument. Call f() with a D21
+and a D22.
+
+*/
+
+using namespace Res_lib;
 int main()
-{
-  using namespace Graph_lib;
+	{
+		cout << "kecsek" << '\n';
 
-    int xmax = 1000;
-    int ymax = 800;
-
-    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
-
+	B1 kecske(5,7);
+	
+	kecske.f();
 
 
-/*
-    Lines grid;
-    
-    int a = ymax / 8;
-    int b = xmax / 8;
-
-    for (int i=1; i < 8; i++) 
-        grid.add(Point(0, i*100),Point(800, i*100));   
-    for (int i=1; i < 9; i++) 
-        grid.add(Point(i*100, 0),Point(i*100, 800));   
-
-    win.attach(grid); 
-  */
-
-/*
-
-Vector_ref<Rectangle> vr;  // use like vector
-                    // but imagine that it holds references to objects
-for (int i = 0; i<16; ++i) {        // i is the horizontal coordinate
-    for (int j = 0; j<16; ++j) {    // j is the vertical coordinate
-        vr.push_back(new Rectangle(Point(i*20,j*20),20,20));
-        vr[vr.size()-1].set_fill_color(i*16+j);
-        win.attach(vr[vr.size()-1]);
-    }
-
-}
-
-*/
-
-
-
-    Vector_ref<Rectangle> vr;  
-    for (int i = 0; i<8; ++i) {        
-        for (int j = 0; j<8; ++j) {   
-            vr.push_back(new Rectangle(Point(i*100,j*100),100,100));
-            if (i == j){
-                vr[vr.size()-1].set_fill_color(Color::red);
-            }
-            win.attach(vr[vr.size()-1]);
-    }
-    }
-
-    /*
-    Image ii1 {Point(400,0),"car.jpg"};
-    win.attach(ii1);
-    Image ii2 {Point(200,600),"car.jpg"};
-    win.attach(ii2);
-    Image ii3 {Point(600,200),"car.jpg"};
-    win.attach(ii3);
-
-    */
-    int x = 0;
-    int y = 0;
-    Image ii4 {Point(x,y),"car1.jpg"};
-    win.attach(ii4);
-    
-    int a = 5;
-       
-    
-
-    while (a < 6)
-        {
-             win.wait_for_button();
-             x += 100; 
-             if (x > 700){
-                y += 100;
-                x = 0;
-            }
-             ii4.move(x,y);
-
-        }
-
-        
-    
-
+	return 0;
 }
